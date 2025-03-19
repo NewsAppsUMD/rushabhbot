0e437b461cf48f0485c2f54c94d13b53c8656974
rushabhkamdar44 <rkamdar2@terpmail.umd.edu>
2025-03-19 19:58:15 +0000

    added some code, url, and added slack secret

--- slack_bot.py
+++ slack_bot.py
@@ -1,21 +1,10 @@
 import os
-import requests
-from bs4 import BeautifulSoup
 from slack import WebClient
 from slack.errors import SlackApiError
 
 slack_token = os.environ.get('SLACK_API_TOKEN')
+
 client = WebClient(token=slack_token)
-
-orioles_trans_url = "https://www.spotrac.com/mlb/transactions/_/start/2025-01-01/end/2025-12-31/team/bal"
-r = requests.get(orioles_trans_url)
-soup = BeautifulSoup(r.text, 'html.parser')
-print(soup.prettify())
-
-
-
-
-
 msg = sentence
 try:
     response = client.chat_postMessage(
