7814d48b6f70c8c3f8b5c5d40adaa56e9f18fb5c
rushabhkamdar44 <rkamdar2@terpmail.umd.edu>
2025-03-24 23:45:58 +0000

    added some code

--- data.py
+++ data.py
@@ -7,14 +7,19 @@ from slack.errors import SlackApiError
 slack_token = os.environ.get('SLACK_API_TOKEN')
 client = WebClient(token=slack_token)
 
-orioles_trans_url = "https://www.spotrac.com/mlb/transactions/_/start/2025-01-01/end/2025-12-31/team/bal"
-r = requests.get(orioles_trans_url)
-soup = BeautifulSoup(r.text, 'html.parser')
-print(soup.prettify())
+orioles_transactions_url = "https://www.spotrac.com/mlb/transactions/_/start/2025-01-01/end/2025-12-31/team/bal"
+response = requests.get(orioles_transactions_url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:121.0) Gecko/20100101 Firefox/121.0'})
+soup = BeautifulSoup(response.text, 'html.parser')
 
+orioles_transactions = []
 
+table = soup.find('ul', attrs = {'class': 'list-group'})
+for row in table.findAll('ul', attrs = {'class': 'ul'}):
+    orioles_contracts = {}
+    orioles_contracts['player url'] = row.a['href']
+    orioles_transactions.append(orioles_contracts)
 
-
+sentence = f"The O's have made a roster move, here it is: {orioles_contracts['player url']}"
 
 msg = sentence
 try:
